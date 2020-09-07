7a0971d6d8f73df738415ce3ec90ee87b6b0432e
rachel-os <rachel.otete@gmail.com>
2020-09-07 17:04:31 +0200

    add method get Full name

--- src/models/profiles.ts
+++ src/models/profiles.ts
@@ -5,17 +5,22 @@ export interface IProfile extends Document {
   email: string;
   lastname: string;
   firstname: string;
+  getFullName: () => string;
+  setPassword: (password: string)  => void;
+  checkPassword: (password : string) => boolean;
 }
 
 //je définis des contraintes sur ma DB grâce à mongoose 
 const profileSchema = new Schema ({
   email: { type: String, required: true, unique: true},
   lastname: { type: String, required: true},
-  firstname: { type: String, required: true}
+  firstname: { type: String, required: true},
+  password: { type: String, required: true}
 });
+
+profileSchema.methods.getFullname = function () {
+  return `${this.firstname} ${this.lastname}`
+}
 
 //je lie mon interface de code (l.3) avec mongoDB que l'on doit stocker dans collection "profile" de moongoose
 export const Profile = model<IProfile, Model<IProfile>>("profile", profileSchema)
-
-//je peux obtenir une instance 
-const user = new Profile ({})
