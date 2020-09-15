353ce305227e262454e2bcdddb17a2453fa3636e
rachel-os <rachel.otete@gmail.com>
2020-09-15 09:53:11 +0200

    add patch and delete profile route and fix typo

--- src/routes/profileRoute.ts
+++ src/routes/profileRoute.ts
@@ -1,14 +1,40 @@
 import { Request, Response, Router } from 'express';
 import { IProfile, Profile } from '../models/profiles';
 import { authenticationRequired } from '../middlewares/authenticationRequired';
-import { getAllProfiles, getProfile } from '../controllers/profiles';
+import { getAllProfiles, getProfile, updateProfile } from '../controllers/profiles';
 
 const router = Router();
 
-router.get("/me", authenticationRequired, (request: Request, response: Response) => {
-  if(!request.user) { return response.status(401).send() }
-  return response.json((request.user as IProfile).getSafeProfile());
+router.get("/me", authenticationRequired, (req: Request, res: Response) => {
+  if(!req.user) { return res.status(401).send() }
+  return res.json((req.user as IProfile).getSafeProfile());
 });
+
+router.patch("/"), authenticationRequired, (req: Request, res: Response) => {
+  if(!req.user) { return res.status(401).send() }
+  const { email, firstname, lastname, password } = req.body;
+
+  updateProfile(req.user as IProfile, email, firstname, lastname, password)
+  .then(profile => {
+    if(!profile) return res.status(404).send('Profile not found.');
+    return res.status(200).send(profile.getSafeProfile());
+  })
+  .catch(error => {
+    console.log(error);
+    return res.status(500).send();
+  });
+}
+
+// suppression d'un profil
+router.delete('/', authenticationRequired, (req: Request, res: Response) => {
+if(!req.user) { return res.status(401).send() }
+(req.user as IProfile).deleteOne()
+    .then(_profile => res.status(200).send('User deleted.'))
+    .catch(error => {
+     console.error(error);
+    return res.status(500).send();
+    });
+})
 
 // création d'un nouveau profil
 router.post('/', (req: Request, res: Response) => {
